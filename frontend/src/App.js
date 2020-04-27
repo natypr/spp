4cf453ba2f927f3f988abff24cb4d7e1e4b92c6b
Nataliya Statkevich <nataliya_statkevich@epam.com>
2020-04-27 21:36:07 +0300

    News component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,40 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter} from 'react-router-dom';
-
+import {BrowserRouter, Redirect, Route, Switch} from 'react-router-dom';
+import Navbar from "./component/navbar/Navbar";
+import NewsList from "./component/news/NewsList";
+import {Routes} from "./constant/Routes";
+import CreateNews from "./component/news/CreateNews";
+import Registration from "./component/registration/Registration";
+import {AuthContext} from "./component/AuthProvider";
+import Login from "./component/login/Login";
 
 class App extends React.Component {
     render() {
         return (
             <div className="App">
                 <BrowserRouter>
-
-
+                    <Navbar/>
+                    {this.context.currentUser ?
+                        <Switch>
+                            <Route exact path={Routes.newsCreate} component={CreateNews}/>
+                            <Route exact path={Routes.news} component={NewsList}/>
+                            <Route exact path={Routes.login} component={Login}/>
+                            <Route exact path={Routes.registration} component={Registration}/>
+                            <Redirect to={Routes.news}/>
+                        </Switch>
+                        :
+                        <Switch>
+                            <Route exact path={Routes.news} component={NewsList}/>
+                            <Route exact path={Routes.login} component={Login}/>
+                            <Route exact path={Routes.registration} component={Registration}/>
+                            <Redirect to={Routes.login}/>
+                        </Switch>
+                    }
                 </BrowserRouter>
             </div>
         );
     }
 };
-
+App.contextType = AuthContext;
 export default App;
